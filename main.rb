71110f4bd24d7276a98a29765e972b2b9342e948
Folgue02 <davidfolru@gmail.com>
2023-10-10 21:58:31 +0200

    Initial commit

---
+++ main.rb
@@ -0,0 +1,166 @@
+#!/bin/env ruby
+require 'stringio'
+
+class QuestionBuilder
+  def initialize(prompt)
+    @prompt = prompt
+    @right = nil
+    @answers = []
+  end
+
+  def add_answer(answer)
+    answer.strip!
+
+    if answer.start_with? '@'
+      if !@right.nil?
+        raise ArgumentError, "Redefining the index for the right question (prompt: #{@prompt}, current answer: #{answer}, previous right index: #{@right}"
+      end
+
+      answer.slice!(0)
+      @right = @answers.length
+    end
+
+    @answers << answer
+  end
+
+  def build
+    if @right.nil?
+      raise ArgumentError, "No right answer for question #{@prompt}"
+    end
+    question = Question.new(@prompt, @answers, @right)
+    puts question
+    return question
+  end
+end
+
+class Question
+  def initialize(question_prompt, answers = [], right = 0)
+    @question_prompt = question_prompt
+    @answers = answers
+    @right = right
+
+    if @answers.length <= @right
+      raise ArgumentError, "#{@right} is out of bounds for answers with length #{@answers.length}"
+    end
+  end
+
+  def display(index=nil)
+    if !index.nil?
+      print "#{index}. "
+    end
+    puts " :: #{@question_prompt}"
+
+    count = 1
+    @answers.each do |answer|
+      puts " [#{count}]: #{answer}"
+      count += 1
+    end
+
+  end
+
+
+  def prompt(index = nil)
+    self.display index
+
+    choice = -1 
+    while choice < 1 || choice > @answers.length
+      print "Answer: "
+      choice = STDIN.gets.chomp.to_i
+
+      if choice > @answers.length
+        puts "Invalid answer (range: 0, #{@answers.length})"
+      end
+    end
+
+    return @right == choice - 1
+  end
+
+  def to_s
+    sb = StringIO.new
+    sb.puts "#{@question_prompt} (RIGHT QUESTION INDEX: #{@right})"
+
+    count = 1
+    @answers.each do |answer|
+      sb.puts " #{count}: #{answer}"
+      count += 1
+    end
+    
+    return sb.string
+  end
+
+  attr_reader :question_prompt, :answers, :right
+end
+
+class QuestionBundle
+  def initialize(file_path)
+    @questions = []
+
+    current_question = nil
+
+    File.foreach(file_path) do |line|
+      if blank_string?(line) 
+        next
+      elsif line.start_with? " "
+        if current_question.nil?
+          raise ArgumentError, "The file starts with an answer (#{line}) instead of a prompt."
+        end
+        current_question.add_answer line
+      else
+        if !current_question.nil?
+          @questions << current_question.build
+          current_question = nil
+        end
+        current_question = QuestionBuilder.new line 
+      end
+    end
+    if !current_question.nil?
+      @questions << current_question.build
+    end
+  end
+
+  def prompt_all
+    score = 0
+
+    count = 0
+    @questions.each do |question|
+      count += 1
+      if question.prompt count
+        score += 1
+      end
+    end
+
+    return score
+  end
+
+  def to_s
+    sb = StringIO.new
+
+    @questions.each do |question|
+      sb.puts question.to_s
+      sb.puts "" 
+    end
+
+    return sb.string
+  end
+
+  def number_of_questions
+    @questions.length
+  end
+end
+
+def blank_string?(text)
+  return text.strip == ""
+end
+
+def main
+  if ARGV.length < 1
+    puts "No file specified :("
+  else
+    bdl = QuestionBundle.new ARGV[0]
+    puts bdl
+    puts "Right questions: #{bdl.prompt_all}/#{bdl.number_of_questions}"
+  end
+end
+
+
+main()
